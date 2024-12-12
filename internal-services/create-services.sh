eefc4e048de35edc166723b524db4fbb4c2d87f7
MiguelCastilloSanchez <A18003592@alumnos.uady.mx>
2024-12-12 16:21:39 -0600

    #patch to sh file

--- internal-services/create-services.sh
+++ internal-services/create-services.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 kubectl create namespace app-test
 
-helm install my-mongodb-auth-service bitnami/mongodb -f mongodb-values-auth-service.yaml -n app-test
-helm install my-mongodb-users-service bitnami/mongodb -f mongodb-values-users-service.yaml -n app-test
-helm install my-mongodb-posts-service bitnami/mongodb -f mongodb-values-posts-service.yaml -n app-test
+helm install my-mongodb-auth-service bitnami/mongodb -f ./internal-services/mongodb-values-auth-service.yaml -n app-test
+helm install my-mongodb-users-service bitnami/mongodb -f ./internal-services/mongodb-values-users-service.yaml -n app-test
+helm install my-mongodb-posts-service bitnami/mongodb -f ./internal-services/mongodb-values-posts-service.yaml -n app-test
 
-helm install my-rabbitmq bitnami/rabbitmq -f rabbitmq-values.yaml -n app-test
+helm install my-rabbitmq bitnami/rabbitmq -f ./internal-services/rabbitmq-values.yaml -n app-test
 
-helm install my-redis bitnami/redis -f redis-values.yaml -n app-test
+helm install my-redis bitnami/redis -f ./internal-services/redis-values.yaml -n app-test
